be6cc591e12691e48a13d8e1fa7d5d1c78fd96e6
aroum <aroum24@gmail.com>
2023-11-14 23:52:33 +0300

    undef

--- config/boards/shields/kabarga/status_led.c
+++ config/boards/shields/kabarga/status_led.c
@@ -31,7 +31,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define LED_STATUS_ON 1
 #define LED_STATUS_OFF 0
 
-struct led {
+struct led
+{
     const struct device *gpio_dev;
     unsigned int gpio_pin;
     unsigned int gpio_flags;
@@ -43,7 +44,12 @@ struct led status_led = {
     .gpio_flags = GPIO_OUTPUT | DT_GPIO_FLAGS(STATUS_LED_NODE, gpios),
 };
 
-enum { BAT_1, BAT_2, BAT_3 };
+enum
+{
+    BAT_1,
+    BAT_2,
+    BAT_3
+};
 struct led battery_leds[] = {
     [BAT_1] = {
         .gpio_dev = DEVICE_DT_GET(DT_GPIO_CTLR(BAT_LED_NODE_1, gpios)),
@@ -66,16 +72,20 @@ static inline void ledON(const struct led *led) { gpio_pin_set(led->gpio_dev, le
 
 static inline void ledOFF(const struct led *led) { gpio_pin_set(led->gpio_dev, led->gpio_pin, LED_STATUS_OFF); }
 
-static void led_all_OFF() {
+static void led_all_OFF()
+{
     ledOFF(&status_led);
-    for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++) {
+    for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
+    {
         ledOFF(&battery_leds[i]);
     }
 };
 
-void led_configure(const struct led *led) {
+void led_configure(const struct led *led)
+{
     int ret = gpio_pin_configure(led->gpio_dev, led->gpio_pin, led->gpio_flags);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         printk("Error %d: failed to configure pin %d\n", ret, led->gpio_pin);
         return;
     }
@@ -83,32 +93,41 @@ void led_configure(const struct led *led) {
     ledOFF(led);
 }
 
-void blink(const struct led *led, uint32_t sleep_ms, const int count) {
-    for (int i = 0; i < count; i++) {
+void blink(const struct led *led, uint32_t sleep_ms, const int count)
+{
+    for (int i = 0; i < count; i++)
+    {
         ledON(led);
         k_msleep(sleep_ms);
         ledOFF(led);
         k_msleep(sleep_ms);
     }
 }
-void blink_once(const struct led *led, uint32_t sleep_ms) {
+void blink_once(const struct led *led, uint32_t sleep_ms)
+{
     ledON(led);
     k_msleep(sleep_ms);
     ledOFF(led);
 }
 
-void display_battery(void) {
+void display_battery(void)
+{
     uint8_t level = bt_bas_get_battery_level();
     LOG_WRN("Battery %d", level);
 
-    if (level <= 20) {
+    if (level <= 20)
+    {
         blink(&battery_leds[0], LED_BATTERY_BLINK, 5);
-    } else {
+    }
+    else
+    {
         ledON(&battery_leds[0]);
-        if (level > 40) {
+        if (level > 40)
+        {
             ledON(&battery_leds[1]);
         }
-        if (level > 80) {
+        if (level > 80)
+        {
             ledON(&battery_leds[2]);
         }
     }
@@ -119,48 +138,58 @@ void display_battery(void) {
 
 /**
  * Running charging animation
-*/
+ */
 struct k_timer bat_timer;
 int led_bat_working = 0;
-void led_bat_animation() {
+void led_bat_animation()
+{
 
     enum zmk_usb_conn_state usb_status = zmk_usb_get_conn_state();
-    if (usb_status == ZMK_USB_CONN_NONE) {
+    if (usb_status == ZMK_USB_CONN_NONE)
+    {
         return;
     }
 
     uint8_t level = bt_bas_get_battery_level();
 
-    if (level < 40) {
+    if (level < 40)
+    {
         ledON(&battery_leds[0]);
         ledOFF(&battery_leds[1]);
         ledOFF(&battery_leds[2]);
         k_msleep(LED_BATTERY_SLEEP_SHOW);
         ledOFF(&battery_leds[0]);
-    } else if (level < 80) {
+    }
+    else if (level < 80)
+    {
         ledON(&battery_leds[0]);
         ledON(&battery_leds[1]);
         ledOFF(&battery_leds[2]);
         k_msleep(LED_BATTERY_SLEEP_SHOW);
         ledOFF(&battery_leds[1]);
-    } else if (level > 80 && level < 100) {
+    }
+    else if (level > 80 && level < 100)
+    {
         ledON(&battery_leds[0]);
         ledON(&battery_leds[1]);
         ledON(&battery_leds[2]);
         k_msleep(LED_BATTERY_SLEEP_SHOW);
         ledOFF(&battery_leds[2]);
-    } else {
+    }
+    else
+    {
         ledON(&battery_leds[0]);
         ledON(&battery_leds[1]);
         ledON(&battery_leds[2]);
     }
-    
-    k_timer_start(&bat_timer, K_SECONDS(LED_BATTERY_SLEEP_SHOW/1000), K_NO_WAIT);
+
+    k_timer_start(&bat_timer, K_SECONDS(LED_BATTERY_SLEEP_SHOW / 1000), K_NO_WAIT);
 }
 void led_bat_handler(struct k_work *work)
 {
     enum zmk_activity_state state = zmk_activity_get_state();
-    if (state == ZMK_ACTIVITY_ACTIVE) {
+    if (state == ZMK_ACTIVITY_ACTIVE)
+    {
         return;
     }
 
@@ -174,41 +203,55 @@ void led_bat_timer_handler(struct k_timer *dummy)
 }
 K_TIMER_DEFINE(bat_timer, led_bat_timer_handler, NULL);
 
-
 /**
  * Checking the connection status
-*/
+ */
 struct k_timer led_timer;
+#if undefined(CONFIG_BOARD_HARPER_LEFT)
+bool led_conn_check_working = false;
+#else
+bool peripheral_ble_connected = false;
+#endif
 
-    bool led_conn_check_working = false;
-
-
-void check_ble_connection() {
-
- 
-        if (zmk_ble_active_profile_is_connected()) {
-            led_conn_check_working = false;
-
-        } else {
-            enum usb_dc_status_code usb_status = zmk_usb_get_status();
-            if (usb_status == USB_DC_CONNECTED) {
-                return;
-            }
-
-            blink_once(&status_led, LED_BLINK_CONN);
-            led_conn_check_working = true;
-            // Restart timer for next status check
-            k_timer_start(&led_timer, K_SECONDS(4), K_NO_WAIT);
-        }
-
-void led_check_connection_handler(struct k_work *work)
+void check_ble_connection()
 {
 
-        enum zmk_activity_state state = zmk_activity_get_state();
-        if (state != ZMK_ACTIVITY_ACTIVE) {
+#if defined(CONFIG_BOARD_HARPER_LEFT)
+    if (zmk_ble_active_profile_is_connected())
+    {
+        led_conn_check_working = false;
+    }
+    else
+    {
+        enum usb_dc_status_code usb_status = zmk_usb_get_status();
+        if (usb_status == USB_DC_CONNECTED)
+        {
             return;
         }
 
+        blink_once(&status_led, LED_BLINK_CONN);
+        led_conn_check_working = true;
+        // Restart timer for next status check
+        k_timer_start(&led_timer, K_SECONDS(4), K_NO_WAIT);
+    }
+#else
+    if (peripheral_ble_connected)
+    {
+        return;
+    }
+    blink_once(&status_led, LED_BLINK_CONN);
+    k_timer_start(&led_timer, K_SECONDS(4), K_NO_WAIT);
+#endif
+}
+void led_check_connection_handler(struct k_work *work)
+{
+#if undefined(CONFIG_BOARD_HARPER_LEFT)
+    enum zmk_activity_state state = zmk_activity_get_state();
+    if (state != ZMK_ACTIVITY_ACTIVE)
+    {
+        return;
+    }
+#endif
 
     check_ble_connection();
 }
@@ -220,76 +263,102 @@ void led_timer_handler(struct k_timer *dummy)
 }
 K_TIMER_DEFINE(led_timer, led_timer_handler, NULL);
 
-
-static int led_init(const struct device *dev) {
+static int led_init(const struct device *dev)
+{
 
     led_configure(&status_led);
 
-    for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++) {
+    for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
+    {
         led_configure(&battery_leds[i]);
     }
 
     display_battery();
-    // check_ble_connection();
+    check_ble_connection();
 
     return 0;
 }
 
 SYS_INIT(led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
 
-
 /**
  * Show leds on profile changing
-*/
-
-    int led_profile_listener(const zmk_event_t *eh)
+ */
+#if undefined(CONFIG_BOARD_HARPER_LEFT)
+int led_profile_listener(const zmk_event_t *eh)
+{
+    const struct zmk_ble_active_profile_changed *profile_ev = NULL;
+    if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL)
     {
-        const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-        if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
-            return ZMK_EV_EVENT_BUBBLE;
-        }
-
-        /*
-        For profiles 1-3 blink appropriate leds.
-        For other profiles just blink blue
-        */
-        if (profile_ev->index <= 2) {
-            for (int i = 0; i <= profile_ev->index; i++) {
-                ledON(&battery_leds[i]);
-            }
-            k_msleep(LED_BLINK_PROFILE);
-            led_all_OFF();
-        } else {
-            blink_once(&status_led, LED_BLINK_PROFILE);
-        }
-
-        if (!led_conn_check_working) {
-            check_ble_connection();
-        }
-
         return ZMK_EV_EVENT_BUBBLE;
     }
 
-    ZMK_LISTENER(led_profile_status, led_profile_listener)
-    ZMK_SUBSCRIPTION(led_profile_status, zmk_ble_active_profile_changed);
+    /*
+    For profiles 1-3 blink appropriate leds.
+    For other profiles just blink blue
+    */
+    if (profile_ev->index <= 2)
+    {
+        for (int i = 0; i <= profile_ev->index; i++)
+        {
+            ledON(&battery_leds[i]);
+        }
+        k_msleep(LED_BLINK_PROFILE);
+        led_all_OFF();
+    }
+    else
+    {
+        blink_once(&status_led, LED_BLINK_PROFILE);
+    }
 
+    if (!led_conn_check_working)
+    {
+        check_ble_connection();
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(led_profile_status, led_profile_listener)
+ZMK_SUBSCRIPTION(led_profile_status, zmk_ble_active_profile_changed);
+#else
+int led_profile_listener(const zmk_event_t *eh)
+{
+    const struct zmk_split_peripheral_status_changed *status = as_zmk_split_peripheral_status_changed(eh);
+
+    peripheral_ble_connected = status->connected;
+    if (!peripheral_ble_connected)
+    {
+        check_ble_connection();
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(led_profile_status, led_profile_listener)
+ZMK_SUBSCRIPTION(led_profile_status, zmk_split_peripheral_status_changed);
+#endif
 
 /**
  * Restore activity after return to active state
-*/
+ */
 int led_state_listener(const zmk_event_t *eh)
 {
     enum zmk_activity_state state = zmk_activity_get_state();
 
+#if undefined(CONFIG_BOARD_HARPER_LEFT)
+    if (state == ZMK_ACTIVITY_ACTIVE && !led_conn_check_working)
+    {
+        check_ble_connection();
+    }
+#endif
 
-        if (state == ZMK_ACTIVITY_ACTIVE && !led_conn_check_working) {
-            check_ble_connection();
-        }
-
-
-    if (state != ZMK_ACTIVITY_ACTIVE) {
+    if (state != ZMK_ACTIVITY_ACTIVE)
+    {
         led_bat_animation();
-    } else {
+    }
+    else
+    {
         led_all_OFF();
     }
 
