1c0e291e67a8462e0a98fe2049b5dcadaa51871d
aroum <aroum24@gmail.com>
2024-02-11 14:56:54 +0300

    Update status_led.c

--- config/boards/shields/kabarga/status_led.c
+++ config/boards/shields/kabarga/status_led.c
@@ -309,11 +309,11 @@ void led_timer_handler(struct k_timer *dummy)
 }
 K_TIMER_DEFINE(led_timer, led_timer_handler, NULL);
 
-K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 void my_timer_handler(struct k_timer *dummy)
 {
     k_work_submit(&my_work);
 }
+K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
 void my_work_handler(struct k_work *work)
 {
