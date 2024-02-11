fbb599e556c253a60fc44f2b389cd3b0c287dfd4
aroum <aroum24@gmail.com>
2024-02-11 19:50:20 +0300

    Update status_led.c

--- config/boards/shields/kabarga/status_led.c
+++ config/boards/shields/kabarga/status_led.c
@@ -356,7 +356,7 @@ static int led_init(const struct device *dev)
     {
         led_configure(&battery_leds[i]);
     }
-    k_timer_start(&bat_show_once_timer, K_NO_WAIT, K_SECONDS(10));
+    k_timer_start(&bat_show_once_timer, K_NO_WAIT, K_SECONDS(1));
     check_ble_connection();
     return 0;
 }
