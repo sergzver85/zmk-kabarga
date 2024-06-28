f49e6b2d43f0aef78354bbcd02201f667e2ecc52
aroum <aroum24@gmail.com>
2024-06-28 14:54:02 +0300

    status LED V2 + smart sleep

--- config/boards/shields/kabarga/status_led.c
+++ config/boards/shields/kabarga/status_led.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #include <zmk/activity.h>
-#include <zmk/activity.h>
 #include <zmk/battery.h>
 #include <zmk/ble.h>
 #include <zmk/endpoints.h>
@@ -40,7 +39,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define LED_STATUS_OFF 0
 
 #define disable_led_sleep_pc
-#define real_bat_animation
+// #define real_bat_animation
 #define show_bat_status_all_time
 #define show_led_idle
 
@@ -86,6 +85,7 @@ static inline void ledON(const struct led *led)
 {
     gpio_pin_set(led->gpio_dev, led->gpio_pin, LED_STATUS_ON);
 }
+
 static inline void ledOFF(const struct led *led)
 {
     gpio_pin_set(led->gpio_dev, led->gpio_pin, LED_STATUS_OFF);
@@ -98,7 +98,7 @@ static void led_all_OFF()
     {
         ledOFF(&battery_leds[i]);
     }
-};
+}
 
 void led_configure(const struct led *led)
 {
@@ -145,8 +145,8 @@ void led_bat_animation()
 #endif
 
 #ifdef disable_led_sleep_pc
-    enum usb_dc_status_code usb_status_suspend_ = zmk_usb_get_status();
-    if (usb_status_suspend_ == USB_DC_SUSPEND)
+    enum usb_dc_status_code usb_status_suspend = zmk_usb_get_status();
+    if (usb_status_suspend == USB_DC_SUSPEND)
     {
         led_all_OFF();
         return;
@@ -155,7 +155,6 @@ void led_bat_animation()
 
 #ifdef real_bat_animation
     uint8_t level = zmk_battery_state_of_charge();
-    // LOG_WRN("Battery %d", level);
 
     switch (led_i)
     {
@@ -203,27 +202,15 @@ void led_bat_animation()
         break;
     }
 #else
-    switch (led_i)
-    {
-    case 1:
-        ledON(&battery_leds[0]);
-        led_i++;
-        break;
-    case 2:
-        ledON(&battery_leds[1]);
-        led_i++;
-        break;
-    case 3:
-        ledON(&battery_leds[2]);
-        led_i = 0;
-        break;
-    case 0:
+        for (int i = 0; i <= 3; i++)
+        {
+            ledON(&battery_leds[i]);
+            k_msleep(LED_BATTERY_BLINK);
+        }
         led_all_OFF();
-        led_i++;
-        break;
-    }
+        return;
 #endif
-    k_timer_start(&bat_timer, K_SECONDS(LED_BATTERY_SLEEP_SHOW / 1000), K_NO_WAIT);
+    // k_timer_start(&bat_timer, K_SECONDS(LED_BATTERY_SLEEP_SHOW / 1000), K_NO_WAIT);
 }
 
 void led_bat_handler(struct k_work *work)
@@ -233,13 +220,13 @@ void led_bat_handler(struct k_work *work)
     {
         return;
     }
-    led_bat_animation();
+    // led_bat_animation();
 }
 K_WORK_DEFINE(led_bat_worker, led_bat_handler);
 
 void led_bat_timer_handler(struct k_timer *dummy)
 {
-    k_work_submit(&led_bat_worker);
+    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &led_bat_worker);
 }
 K_TIMER_DEFINE(bat_timer, led_bat_timer_handler, NULL);
 
@@ -282,7 +269,7 @@ K_WORK_DEFINE(led_check_conn, led_check_connection_handler);
 
 void led_timer_handler(struct k_timer *dummy)
 {
-    k_work_submit(&led_check_conn);
+    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &led_check_conn);
 }
 K_TIMER_DEFINE(led_timer, led_timer_handler, NULL);
 
@@ -290,7 +277,7 @@ void bat_show_once_work_handler(struct k_work *work);
 K_WORK_DEFINE(bat_show_once_work, bat_show_once_work_handler);
 void bat_show_once_timer_handler(struct k_timer *dummy)
 {
-    k_work_submit(&bat_show_once_work);
+    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &bat_show_once_work);
 }
 K_TIMER_DEFINE(bat_show_once_timer, bat_show_once_timer_handler, NULL);
 void bat_show_once_work_handler(struct k_work *work)
@@ -346,7 +333,7 @@ void bat_show_once_work_handler(struct k_work *work)
     }
     else
     {
-        // NOTE(sqd): Basically timer will go on and on until we get level different that zero.
+        // NOTE: Basically timer will go on and on until we get level different that zero.
     }
 }
 
@@ -407,7 +394,7 @@ int led_state_listener(const zmk_event_t *eh)
 #ifdef show_led_idle
     if (state != ZMK_ACTIVITY_ACTIVE)
     {
-        led_bat_animation();
+        // led_bat_animation();
     }
     else
     {
@@ -422,38 +409,33 @@ int led_state_listener(const zmk_event_t *eh)
 ZMK_LISTENER(led_activity_state, led_state_listener)
 ZMK_SUBSCRIPTION(led_activity_state, zmk_activity_state_changed);
 
-// https://github.com/zmkfirmware/zmk/blob/5826b80374625d448cfbfc739dde4fda1e6f2681/app/src/usb.c#L33
+int led_usb_conn_listener(const zmk_event_t *eh)
+{
+    const struct zmk_usb_conn_state_changed *usb_ev = NULL;
+    if ((usb_ev = as_zmk_usb_conn_state_changed(eh)) == NULL)
+    {
+        return ZMK_EV_EVENT_BUBBLE;
+    }
 
-// ToDO: add KeyCode for display battery
-// https://github.com/zmkfirmware/zmk/blob/b8846cf6355c5d7ae52a191988054b532a264f0c/app/dts/behaviors/reset.dtsi#L12
-// https://github.com/zmkfirmware/zmk/blob/b8846cf6355c5d7ae52a191988054b532a264f0c/app/src/behaviors/behavior_reset.c
+    // if (usb_ev->conn_state == ZMK_USB_CONN_POWERED || usb_ev->conn_state == ZMK_USB_CONN_HID)
+    if (usb_ev->conn_state == ZMK_USB_CONN_POWERED)
 
-// O OFF
-// B BLINK
-// X ON
-// -------------------
-// OBBO BOOTLOADER
-// -------------------
-// Bluetooth
-// XOOO BT1
-// OXOO BT2
-// OOXO BT3
-// OOOX Connection lost
-// -------------------
-// CHARGING
-// -------------------
-// VAR1 animation, without bat status
-// -------------------
-// XOOO
-// XXOO
-// XXXO
-// OOOO
-// -------------------
-// VAR2
-// -------------------
-// BBBO 100%
-// XXXO >70%
-// XXBO >50%
-// XBOO >30%
-// XOOO >15%
-// BOOO <15%
+    {
+        led_bat_animation();
+    }
+
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(led_usb_conn_state, led_usb_conn_listener)
+ZMK_SUBSCRIPTION(led_usb_conn_state, zmk_usb_conn_state_changed);
+
+void show_battery()
+{
+    k_work_submit_to_queue(zmk_workqueue_lowprio_work_q(), &bat_show_once_work);
+}
+
+void hide_battery()
+{
+    led_all_OFF();
+}
